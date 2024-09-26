a5abe8231bd3f33a9d4cf7af5f7cdcb721f1fe3f
unknown <anri.maglinte@gmail.com>
2024-09-26 17:16:58 +0800

    .

--- Lab3_Maglinte-Emmanuel.py
+++ Lab3_Maglinte-Emmanuel.py
@@ -1,13 +1,15 @@
-monthly_salary = int(input("Enter Amount of Monthly Salary: "))
+monthly_salary = int(input("Enter Monthly Salary: "))
 loan_amount = int(input("Enter Loan Amount: "))
-valid_loan = monthly_salary * 10
-months = int(input("How many Months of payment: "))
 
-if monthly_salary <= 30000 and loan_amount >= valid_loan:
-    print(months)
-    payment_to_loan = loan_amount * 0.10
-    print("Amount to Pay: ",payment_to_loan)
-    print("Months to Pay: ", months)
+if monthly_salary >= 30000 and loan_amount <= 10 * monthly_salary:
+    print("You are eligible for the loan!")
+    months = int(input("How many months do you want to repay the loan?: "))
+    total_amount = loan_amount * 1.10 #10% interest
+    monthly_payment = total_amount / months
+    print(f"Total amount to repay (with interest): {total_amount:.2f}")
+    print(f"Your monthly payment will be: {monthly_payment:.2f}")
 else:
-    print("Loan Declined")
-
+    if monthly_salary < 30000:
+        print("Sorry, your salary is too low")
+    if loan_amount > 10 * monthly_salary:
+        print("Sorry, your loan is too high")   
